5acf2f3bbf17c1e83e7b5576f4d517aedbfd0a78
Kevin Hang <herezy2006@gmail.com>
2025-06-14 12:32:26 +0200

    navigation on laptop -- ok for now

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router'
+import {SidebarComponent} from "@syncfusion/ej2-react-navigations";
+import { NavItems } from 'components';
 
 const AdminLayout = () => {
 
@@ -8,7 +10,9 @@ const AdminLayout = () => {
     <div className='admin-layout'>
       MobileSidebar
       <aside className='w-full max-w-[270px] hidden lg:block '>
-        Sidebar
+        <SidebarComponent width={270} enableGestures={false}>
+          <NavItems/>
+        </SidebarComponent>
       </aside>
       <aside className='children'>
         <Outlet />
